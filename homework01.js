e263b48f380c5765b3b227f04382644142c8e623
yulopadua <yulo.padua@gmail.com>
2025-07-24 15:02:27 +0800

    submit homework01

---
+++ homework01.js
@@ -0,0 +1,115 @@
+//Task-1
+let task1_1 = `I start to practice "JavaScript" today, and I like it.`;
+console.log(task1_1);
+
+let task1_2 = `The secret of getting ahead is getting started.`;
+console.log(task1_2);
+
+let task1_3 = `"Don't limit yourself."`;
+console.log(task1_3);
+
+let task1_4 = `Invest in your dreams. Grind now. Shine later.`;
+console.log(task1_4);
+
+let task1_5 = `It's not the load that breaks you down, it's the way you carry it.`;
+console.log(task1_5);
+
+let task1_6 = `The hard days are what make you stronger.`;
+console.log(task1_6);
+
+let task1_7 = `You can waste your lives drawing lines. Or you can live your life crossing them.`;
+console.log(task1_7);
+
+
+//Task-2
+let task2 = 
+`\tJavaScript is a high-level programming language primarily used to build web applications. It is not limited to the web; it can also be used for the backend development with technologies like Node.js.\n\n\n\tIt is open source and has a huge community support, which means there are plenty of resources and support available for learning. It uses a syntax like other programming languages and easy to learn.`;
+console.log(task2);
+
+
+//Task-3
+const myAge = 29;
+console.log(myAge);
+
+const myFavoriteNumber = 1;
+console.log(myFavoriteNumber);
+
+const myHeight = `5'7"`;
+console.log(myHeight);
+
+const myWeight = `65kg`;
+console.log(myWeight);
+
+const myFavoriteLetter = `A`;
+console.log(myFavoriteLetter);
+
+
+//Task-4
+const num1 = 25;
+const num2 = 35;
+const sum = num1 + num2;
+console.log(`The sum of the numbers = ${sum}`);
+
+
+//Task-5
+const n1 = 5;
+const n2 = 7;
+const product = n1 * n2;
+console.log(`The product of the numbers = ${product}`);
+
+
+//Task-6
+const number1 = 24;
+const number2 = 10;
+console.log(`The sum of the numbers is = ${number1 + number2}\n
+The product of the numbers is = ${number1 * number2}\n
+The subtraction of the numbers is = ${number1 - number2}\n
+The division of the numbers is = ${number1 / number2}\n
+The remainder of the numbers is = ${number1 % number2}`);
+
+
+//Task-7
+const i1 = 7;
+const i2 = 11;
+const ave_i = (i1 +i2) / 2;
+console.log(`The average of the numbers is: ${ave_i}`);
+
+
+//Task-8
+const a1 = 6, a2 = 10, a3 = 12, a4 = 15, a5 = 17;
+const ave_a = (a1 + a2 + a3 + a4 + a5) / 5;
+console.log(`The average of the numbers is: ${ave_a}`);
+
+
+//Task-9
+const b1 = 5;
+const b2 = 6;
+const b3 = 10;
+console.log(`The 5 multiplied with 5 is = ${b1 * b1}\n
+The 6 multiplied with 6 is = ${b2 * b2}\n
+The 10 multiplied with 10 is = ${b3 * b3}`);
+
+
+//Task-10
+const side = 7;
+console.log(`The perimeter of the square = ${4 * side}\n
+The area of the square = ${side * side}`);
+
+
+//Task-11
+const favBook = "JS Algorithms & Data Structures";
+const favColor ="Blue";
+const favNumber = 7;
+console.log(`The favorite book is ${favBook}\n
+The favorite color is ${favColor}\n
+The favorite number is ${favNumber}`);
+
+
+//Task-12
+const firstName = "John";
+const lastName = "Doe";
+const age = 45;
+const emailAddress = "johndoe@gmail.com";
+const phoneNumber = "(123) 123 1234";
+const address = "123 St Chicago IL 12345";
+console.log(`\tUser who joined this program is ${firstName} ${lastName}. ${firstName}'s age is ${age}. ${firstName}'s email address is ${emailAddress}, phone number is ${phoneNumber}, and address is ${address}.`);
